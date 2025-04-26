74fbea39918baf73219e81ac903f92975a4bb1e5
davidcoats <david@getsafetycone.com>
2025-04-25 17:12:52 -0700

    Initial commit.

---
+++ source/F10Y.NG0003.Construction/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace F10Y.NG0003.Construction
+{
+    public static class Instances
+    {
+		
+    }
+}
---
+++ source/F10Y.NG0003/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace F10Y.NG0003
+{
+	/// <summary>
+	/// System.Text.Json package selector.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
